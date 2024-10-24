bf1b9f62b2efb460e801235acfd7bad3fa6db819
i_Zolox <33046239+izolox@users.noreply.github.com>
2024-10-24 12:10:15 -0500

    hotfix(formatting): fix formatting

--- main.py
+++ main.py
@@ -22,8 +22,6 @@ def main():
     # Create window manager
     window_manager = WindowManager(config.WINDOW_NAME)
     windows = window_manager.get_windows()
-
-    
     
     if not config.DEBUG and (len(windows) < 2):
         print("Not enough windows found, exiting...")
@@ -49,6 +47,5 @@ def main():
         
         time.sleep(config.DEBUG and 10 or (60 * 5))
         
-
 if __name__ == "__main__":
     main()
